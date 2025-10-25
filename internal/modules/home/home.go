352dea9d87aa4988a0b7e6b3a3671624f327f3de
Sony AK <sony@sony-ak.com>
2025-10-25 14:40:30 +0700

    chore: remove swagger annotations and response structs from home handler

--- internal/modules/home/home.go
+++ internal/modules/home/home.go
@@ -8,23 +8,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type Home struct {
-	Version string `json:"version" example:"0.0.1"`
-}
-
-type HomeSuccessResponse struct {
-	Success bool   `json:"success" example:"true"`
-	Message string `json:"message" example:"API is running."`
-	Data    Home   `json:"data"`
-}
-
-// @Summary      Show the home status of API
-// @Description  Get the status of the API
-// @Tags         home
-// @Accept       json
-// @Produce      json
-// @Success      200  {object}  HomeSuccessResponse
-// @Router       / [get]
 func HomeHandler(c *fiber.Ctx) error {
 	return response.SendSuccessResponse(c, "API is running.", fiber.Map{
 		"version": os.Getenv("APP_VERSION"),
